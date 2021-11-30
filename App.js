f061fbecf70d7c3644e2967db0818266db218756
olawale-o <omoogunolawale@gmail.com>
2021-11-30 15:21:05 +0100

    Modify App.js

--- App.js
+++ App.js
@@ -7,25 +7,17 @@
  */
 
 import React from 'react';
-import Artists from './pages/Artist/Artists';
-import Settings from './pages/Setting/Settings';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-// import HeaderTitle from './components/Header';
-const AuthDrawer = createDrawerNavigator();
+import { Provider } from 'react-redux';
+import configureStore from  './redux/configureStore';
+import Main from './pages/Main';
+const store = configureStore;
 
 const App = () => {
-
   return (
-    <NavigationContainer>
-      <AuthDrawer.Navigator>
-        <AuthDrawer.Screen name='Settings' component={Settings} />
-        <AuthDrawer.Screen name='Artists' component={Artists} />
-      </AuthDrawer.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <Main />
+    </Provider>
   );
 };
-
-
 
 export default App;
