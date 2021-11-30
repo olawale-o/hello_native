14e38764ee95748492cd10079469df422bec253d
olawale-o <omoogunolawale@gmail.com>
2021-11-30 15:17:56 +0100

    Add more action creators

--- redux/auth/action_creators.js
+++ redux/auth/action_creators.js
@@ -1,5 +1,11 @@
-import { AUTH_FAILURE, AUTH_LOADING, AUTH_SUCCESS } from './action_types';
+import {
+  AUTH_FAILURE,
+  AUTH_LOADING,
+  AUTH_SUCCESS,
+  AUTH_TOKEN
+} from './action_types';
 
 export const authLoading = () => ({ type: AUTH_LOADING });
 export const authSuccess = (payload) => ({ type: AUTH_SUCCESS, payload });
 export const authFailure = () => ({ type: AUTH_FAILURE });
+export const authToken = (payload) => ({ type: AUTH_TOKEN, payload });
