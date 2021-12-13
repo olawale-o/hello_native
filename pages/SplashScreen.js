8691a3696b5d9eae12e9c944469010b753113915
olawale-o <omoogunolawale@gmail.com>
2021-12-13 16:31:55 +0100

    Create splash screen

---
+++ pages/SplashScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const SplashScreen = () => (
+  <View style={styles.container}>
+    <Text>Splash Page</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default SplashScreen;
