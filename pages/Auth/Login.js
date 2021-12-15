73bf34f392afa65c2ce75dbd9fa9e274f466f002
olawale-o <omoogunolawale@gmail.com>
2021-12-15 22:23:53 +0100

    Improve login screen

--- pages/Auth/Login.js
+++ pages/Auth/Login.js
@@ -8,16 +8,21 @@ import {
   TouchableOpacity,
   Text,
   Platform,
+  Pressable,
 } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+
 import {auth, signInWithEmailAndPassword} from '../../firebase';
 import {authSuccess} from '../../redux/auth/action_creators';
+import Icon from 'react-native-vector-icons/AntDesign';
 
-const isAndroid = () => (Platform.OS === 'android' ? 'height' : 'padding');
+const isAndroid = () => (Platform.OS === 'android' ? 'position' : 'padding');
 
 const Login = () => {
   const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isSelected, setIsSelected] = useState(false);
 
   const handleLogin = () => {
     signInWithEmailAndPassword(auth, email, password)
@@ -28,39 +33,62 @@ const Login = () => {
       .catch(error => console.log(error.message));
   };
   return (
-    <KeyboardAvoidingView style={styles.container} behavior={isAndroid()}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={text => setEmail(text)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          value={password}
-          onChangeText={text => setPassword(text)}
-        />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Login</Text>
+        <Icon name="closecircleo" size={30} color="#86897D" />
       </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Login</Text>
+      <Pressable onPress={() => console.log('press')} style={styles.socialButton}>
+        <View style={styles.socialButtonContainer}>
+          <Icon name="google" size={15} color="#86897D"/>
+          <Text style={styles.buttonText}>Log in with Google</Text>
+        </View>
+      </Pressable>
+      <View style={styles.divider} />
+      <View style={styles.formContainer}>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="wale@test.com"
+            value={email}
+            onChangeText={text => setEmail(text)}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="********"
+            value={password}
+            onChangeText={text => setPassword(text)}
+          />
+        </View>
+        <View style={styles.checkboxContainer}>
+          <CheckBox
+            value={isSelected}
+            onValueChange={(newValue) => setIsSelected(newValue)}
+            style={styles.checkbox}
+          />
+          <Text style={styles.label}>Remember Me</Text>
+        </View>
+        <Pressable onPress={() => console.log('login')} style={styles.loginButton}>
+          <Text style={styles.loginButtonText}>Log in</Text>
+        </Pressable>
+        <View style={styles.forgotPasswordContainer}>
+        <TouchableOpacity onPress={() => console.log('forgot')}>
+          <Text style={styles.linkText}>Forgot Password?</Text>
         </TouchableOpacity>
+        </View>
+        <View style={styles.divider} />
+        <View style={styles.signupContainer}>
+          <Text style={styles.signupText}>Don't have an account?</Text>
+          <TouchableOpacity onPress={() => console.log('forgot')}>
+            <Text style={styles.linkText}>Sign up</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={styles.footerContainer}>
-        <TouchableOpacity
-          style={styles.linkButton}
-          onPress={() => console.log('signup')}>
-          <Text style={styles.linkButtonText}>Don't have an account?</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.linkButton}
-          onPress={() => console.log('signup')}>
-          <Text style={styles.linkButtonText}>Forgot password?</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
+    </View>
   );
 };
 
@@ -68,54 +96,99 @@ export default Login;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    height: '100%',
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  headerText: {
+    fontSize: 20,
+  },
+
+  socialButton: {
+    borderRadius: 10,
+    marginVertical: 25,
+    backgroundColor: '#F3F3F3',
+  },
+
+  socialButtonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    opacity: 0.45,
+    alignItems: 'center',
+    padding: 10,
+  },
+
+  buttonText: {
+    color: '#000',
+    marginLeft: 10,
+    fontWeight: 'bold',
+  },
+
+  divider: {
+    height: 1,
+    backgroundColor: '#eee',
+    marginVertical: 10,
+  },
+
+  formContainer: {
+    paddingVertical: 15,
   },
 
   inputContainer: {
-    width: '80%',
+    paddingVertical: 10,
+  },
+
+  label: {
+    fontWeight: 'bold',
   },
 
   input: {
     backgroundColor: '#fff',
     borderRadius: 10,
     marginTop: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
+    borderWidth: 2,
+    borderColor: '#eee',
+    paddingHorizontal: 10,
   },
 
-  buttonContainer: {
-    width: '60%',
-    marginTop: 10,
-  },
-
-  button: {
-    backgroundColor: '#20232a',
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-  },
-
-  footerContainer: {
-    width: '100%',
+  checkboxContainer: {
     display: 'flex',
-    justifyContent: 'space-between',
+    flexDirection: 'row',
     alignItems: 'center',
+    paddingVertical: 15,
   },
 
-  linkButton: {
-    marginBottom: 10,
+  loginButton: {
+    backgroundColor: '#008AFF',
+    paddingVertical: 10,
+    borderRadius: 15,
   },
 
-  linkButtonText: {
-    color: '#000',
+  loginButtonText: {
+    color: '#fff',
+    textAlign: 'center',
   },
+  
+  forgotPasswordContainer: {
+    paddingVertical: 20,
+  },
+
+  linkText: {
+    color: '#008AFF',
+    textAlign: 'center',
+  },
+
+  signupContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  }
 });
