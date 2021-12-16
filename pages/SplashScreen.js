6daaeea6e3390e874ad43ee91a488285e8d62609
olawale-o <omoogunolawale@gmail.com>
2021-12-16 10:58:49 +0100

    Improve splash screen

--- pages/SplashScreen.js
+++ pages/SplashScreen.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Image} from 'react-native';
 
 const SplashScreen = () => (
   <View style={styles.container}>
-    <Text>Splash Page</Text>
+    <View style={styles.content}>
+      <Image source={require('../assets/images/logo.png')} style={styles.logo} />
+      <Text style={styles.text}>Native</Text>
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 50,
+    height: '100%',
+  },
+
+  content: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  logo: {
+    height: 50,
+    width: 50,
+    resizeMode: 'contain',
+  },
+
+  text: {
+    color: '#002341',
+    fontSize: 32,
   },
 });
 
