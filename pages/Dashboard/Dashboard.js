5c5969456014218983ed061a8f6e730a6827038c
olawale-o <omoogunolawale@gmail.com>
2021-12-22 20:37:01 +0100

    Create Dashboard component

---
+++ pages/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import Map from '../Map/Map';
+import Artists from '../Artist/Artists';
+
+const Drawer = createDrawerNavigator();
+
+const DashBoard = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Artists" component={Artists} options={{ headerShown: false }} />
+      <Drawer.Screen name="Map" component={Map} options={{ headerShown: false }} />
+    </Drawer.Navigator>
+  );
+};
+
+export default DashBoard;
