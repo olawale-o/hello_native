1dc47f5197f60d3b263eefde9756f007dd35ffe8
olawale-o <omoogunolawale@gmail.com>
2021-12-23 09:04:23 +0100

    Fix import error

--- pages/Dashboard/Dashboard.js
+++ pages/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import Map from '../Map/Map';
+import MapScreen from '../Map/MapScreen';
 import Artists from '../Artist/Artists';
 
 const Drawer = createDrawerNavigator();
@@ -9,13 +9,13 @@ const DashBoard = () => {
   return (
     <Drawer.Navigator>
       <Drawer.Screen
-        name="Artists"
-        component={Artists}
+        name="Map"
+        component={MapScreen}
         options={{headerShown: false}}
       />
       <Drawer.Screen
-        name="Map"
-        component={Map}
+        name="Artists"
+        component={Artists}
         options={{headerShown: false}}
       />
     </Drawer.Navigator>
