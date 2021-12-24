dcf467c20f4e6695757403eb5bcd1bab403bb77e
olawale-o <omoogunolawale@gmail.com>
2021-12-24 12:00:20 +0100

    Show user current location on map

--- pages/Map/MapScreen.js
+++ pages/Map/MapScreen.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useEffect, useCallback} from 'react';
+import {StyleSheet, Platform} from 'react-native';
 import MapView from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+import {request, PERMISSIONS} from 'react-native-permissions';
 
 const MapScreen = () => {
+  const [region, setRegion] = React.useState({
+    initialRegion: {
+      latitude: 6.5963,
+      longitude: 3.346,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.015,
+    },
+    error: null,
+  });
+
+  const errorCallBack = useCallback(
+    error => {
+      setRegion({...region, error: error.message});
+    },
+    [region],
+  );
+
+  const successCallBack = useCallback(
+    position => {
+      const {latitude, longitude} = position.coords;
+      setRegion({
+        ...region,
+        initialRegion: {...region.initialRegion, latitude, longitude},
+      });
+    },
+    [region],
+  );
+
+  const getCurrentLocation = useCallback(() => {
+    Geolocation.getCurrentPosition(successCallBack, errorCallBack, {
+      enableHighAccuracy: true,
+      timeout: 20000,
+      maximumAge: 1000,
+    });
+  }, [successCallBack, errorCallBack]);
+
+  const requestLocationPermission = useCallback(async () => {
+    if (Platform.OS === 'android') {
+      const response = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      if (response === 'granted') {
+        getCurrentLocation();
+      }
+    }
+  }, [getCurrentLocation]);
+  // const requestLocationPermission = async () => {
+  //   if (Platform.OS === 'android') {
+  //     const response = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+  //     if (response === 'granted') {
+  //       getCurrentLocation();
+  //     }
+  //   }
+  // };
+
+  useEffect(() => {
+    (async () => {
+      requestLocationPermission();
+    })();
+  }, [requestLocationPermission]);
   return (
     <MapView
       style={styles.mapStyle}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
+      initialRegion={region.initialRegion}
+      showsUserLocation={true}
+      showsMyLocationButton={true}
+      showsPointsOfInterest={false}
+      zoomControlEnabled={true}
     />
   );
 };
@@ -22,5 +81,5 @@ const styles = StyleSheet.create({
   mapStyle: {
     width: '100%',
     height: '100%',
-  }
+  },
 });
