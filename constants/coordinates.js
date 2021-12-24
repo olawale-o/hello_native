6a612f3ad4e2f548c2ee7def2f9f7dd5da29432c
olawale-o <omoogunolawale@gmail.com>
2021-12-24 19:39:00 +0100

    Add dummy coordinates

---
+++ constants/coordinates.js
@@ -0,0 +1,32 @@
+const coordinates = [
+  {
+    latLng: {
+      latitude: 6.6039262,
+      longitude: 3.3327518,
+    },
+    name: 'Ibis hotel ikeja',
+  },
+  {
+    latLng: {
+      latitude: 6.6339725,
+      longitude: 3.2927731,
+    },
+    name: 'Maple wood oko-oba',
+  },
+  {
+    latLng: {   
+      latitude: 6.6342803,
+      longitude: 3.2921889,
+    },
+    name: 'Ifako hospital agege',
+  },
+  {
+    latLng: {
+      latitude: 6.625428,
+      longitude: 3.2872138,
+    },
+    name: 'Arena market Oshodi',
+  },
+];
+
+export default coordinates;
