f1fc38f962fa1fada262176537dc06259a6b2b19
olawale-o <omoogunolawale@gmail.com>
2021-12-30 11:29:06 +0100

    Create CustomSlider

---
+++ components/CustomSlider.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import  { Slider } from '@miblanchard/react-native-slider';
+import { View, StyleSheet } from 'react-native';
+
+const CustomSlider = ({radius, valueChange}) => {
+  return (
+    <View style={styles.radiusSlider}>
+      <Slider
+        value={radius}
+        maximumValue={5000}
+        minimumValue={500}
+        step={radius}
+        onValueChange={valueChange}
+        trackClickable={true}
+      />
+    </View>
+  );
+};
+
+export default CustomSlider;
+
+const styles = StyleSheet.create({
+  radiusSlider: {
+    backgroundColor: '#fff',
+    marginHorizontal: 30,
+  }
+});
