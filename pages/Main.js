25b813c431f515315acef13d2257987147d35b4a
olawale-o <omoogunolawale@gmail.com>
2022-01-30 18:39:33 +0100

    Fix white space

--- pages/Main.js
+++ pages/Main.js
@@ -13,32 +13,52 @@
  import Auth from './Auth/Auth';
  import SplashScreen from './SplashScreen';
  import DashBoard from './Dashboard/Dashboard';
+ import ChatRoom from './Chat/ChatRoom';
  const Stack = createStackNavigator();
  
+ const chatRoomStyle = {
+  headerTitleAlign: 'center',
+  headerStyle: {
+    elevation: 0,
+  },
+ };
+
  const Main = () => {
    return (
      <NavigationContainer>
        <Stack.Navigator>
-         <Stack.Screen
-           name="Splash"
-           component={SplashScreen}
-           options={{headerShown: false}}
-         />
-         <Stack.Screen
-           name="Auth"
-           component={Auth}
-           options={{headerShown: false}}
-         />
-         <Stack.Screen
-           name="Settings"
-           component={Settings}
-           options={{headerShown: false}}
-         />
-         <Stack.Screen
-           name="Dashboard"
-           component={DashBoard}
-           options={{headerShown: false}}
-         />
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Auth"
+          component={Auth}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={Settings}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Dashboard"
+          component={DashBoard}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="ChatRoom"
+          component={ChatRoom}
+          options={({route}) => (
+            {
+              title: route.params.name, id: route.params.id,
+              headerStyle: {
+                elevation: 0,
+              },
+            }
+          )}
+        />
        </Stack.Navigator>
      </NavigationContainer>
    );
